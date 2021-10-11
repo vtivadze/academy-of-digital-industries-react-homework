9ed7ebed077fc73069d56a5342bd5cb5598158b1
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-11 21:16:27 +0400

    Import 'SidebarMenuItem' module from 'index.js'

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,3 +1,4 @@
 export { MenuItem } from "./menuItem.module";
 export { MainMenuItem } from "./mainMenuItem.module";
+export { SidebarMenuItem } from "./sidebarMenuItem.module";
 export { Link } from "./link.module";
