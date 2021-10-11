79782910d611d574c5b014ef929c91264af2e4dd
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-11 21:37:23 +0400

    Realize 'SidebarMenu.js' layout component

--- src/components/layout/SidebarMenu.js
+++ src/components/layout/SidebarMenu.js
@@ -1,11 +1,36 @@
-import sideba
+import React from "react";
+import { sidebarMenuItems as items } from "../../api";
+import { List } from "../menu";
 
-export function SidebarMenu() {
-  return (
-    <div className="sidebar-menu">
-      <ul className="sidebar-menu__container">
-        
-      </ul>
-    </div>
-  );
+export class SidebarMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: items,
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(i) {
+    const items = this.state.items;
+    this.setState({items: items.map((item, index) => {
+      item.isActive = i === index;
+      return item;
+    })});
+  }
+
+  render() {
+    const items = this.state.items;
+
+    return (
+      <div className="sidebar-menu">
+        <ul className="sidebar-menu__container">
+          <List
+            items={ items }
+            handleClick={ this.handleClick }
+          />
+        </ul>
+      </div>
+    );
+  }
 }
