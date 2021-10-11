3e945da8a126adb2e3c7d082f1c99b6c61f7984a
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-11 23:03:44 +0400

    Realize content displaying dinamicly

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import { Header, Main, Footer, Sidebar } from "./components/layout";
 import "./App.css";
+import React from "react";
+import { MainContent } from "./components/content";
 
-function App() {
-  return (
-    <div className="app">
-      <Header />
-      <Sidebar />
-      <Main />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: <MainContent />,
+    }
+  } 
+
+  render() {
+    const content = this.state.content;
+
+    return (
+      <div className="app">
+        <Header />
+        <Sidebar />
+        <Main content={content} />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/content/MainContent.js
@@ -0,0 +1,19 @@
+export function MainContent() {
+  return (
+    <>
+      <h1>React Features</h1>
+      
+      <h2>Declarative</h2>
+      <p>React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.</p>
+      <p>Declarative views make your code more predictable and easier to debug.</p>
+
+      <h2>Component-Based</h2>
+      <p>Build encapsulated components that manage their own state, then compose them to make complex UIs.</p>
+      <p>Since component logic is written in JavaScript instead of templates, you can easily pass rich data through your app and keep state out of the DOM.</p>
+
+      <h2>Learn Once, Write Anywhere</h2>
+      <p>We don’t make assumptions about the rest of your technology stack, so you can develop new features in React without rewriting existing code.</p>
+      <p>React can also render on the server using Node and power mobile apps using React Native.</p>
+    </>
+  );
+}
