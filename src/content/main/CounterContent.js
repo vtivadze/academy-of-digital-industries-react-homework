856178ebfa729bda8aba22c293c1e0f1624b34e3
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-14 12:36:40 +0400

    Add another counter into counter page

--- src/content/main/CounterContent.js
+++ src/content/main/CounterContent.js
@@ -2,16 +2,19 @@ import { Counter } from "../../components";
 
 export const CounterContent = () => {
   return (
-    <div className="section">
+    <div className="section px-0">
       <div className="columns">
-        <div className="column is-one-third">
+        <div className="column is-one-quarter">
           <Counter initialValue={0} initialStep={1} />
         </div>
-        <div className="column is-one-third">
+        <div className="column is-one-quarter">
           <Counter initialValue={100} initialStep={10} />
         </div>
-        <div className="column is-one-third">
+        <div className="column is-one-quarter">
           <Counter initialValue={1000} initialStep={100} />
+        </div>
+        <div className="column is-one-quarter">
+          <Counter initialValue={10000} initialStep={1000} />
         </div>
       </div>
     </div>
