c40d83873d26ead62e2ee6006dbf2ec096c5ca87
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-14 11:46:00 +0400

    Realize Counter

--- src/content/main/CounterContent.js
+++ src/content/main/CounterContent.js
@@ -3,7 +3,17 @@ import { Counter } from "../../components";
 export const CounterContent = () => {
   return (
     <div className="section">
-      <Counter />
+      <div className="columns">
+        <div className="column is-one-third">
+          <Counter initialValue={0} initialStep={1} />
+        </div>
+        <div className="column is-one-third">
+          <Counter initialValue={100} initialStep={10} />
+        </div>
+        <div className="column is-one-third">
+          <Counter initialValue={1000} initialStep={100} />
+        </div>
+      </div>
     </div>
   );
 };
