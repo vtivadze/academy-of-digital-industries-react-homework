11c6e6323d0ba335ea1a8cfb4eff817ea1198e40
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-14 22:20:02 +0400

    Add user's dummy data

--- src/data/index.js
+++ src/data/index.js
@@ -1,2 +1,3 @@
 export { mainMenuItems } from "./mainMenuItems";
 export { sidebarMenuItems } from "./sidebarMenuItems";
+export { users } from "./users";
---
+++ src/data/users.js
@@ -0,0 +1,5 @@
+export const users = [
+  ["John", "Doe", 25],
+  ["Jackie", "Chan", 30],
+  ["Bruce", "Lee", 35],
+];
