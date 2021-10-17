025d791b04fbf6b2213a30df865b94dfdb993468
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-17 12:39:42 +0400

    Add 'LoginForm.js'

--- src/layout/Login.js
+++ src/layout/Login.js
@@ -1,14 +1,12 @@
 export const Login = () => {
   return (
-    // <div className="login">
-      <div className="buttons is-flex-direction-column-reverse mb-0">
-        <a className="button is-primary mr-0 mb-0" href="#">
-          <strong>Sign up</strong>
-        </a>
-        <a className="button is-light is-align-self-stretch mr-0" href="#">
-          Log in
-        </a>
-      </div>
-    // </div>
+    <div className="buttons is-flex-direction-column-reverse mb-0">
+      <a className="button is-primary mr-0 mb-0" href="#">
+        <strong>Sign up</strong>
+      </a>
+      <a className="button is-light is-align-self-stretch mr-0" href="#">
+        Log in
+      </a>
+    </div>
   );
 };
