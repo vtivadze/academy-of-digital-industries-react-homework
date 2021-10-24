ce0fbc18b2a3c92589de4a6dd503ea05a903572e
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-24 11:45:53 +0400

    Homework-5: Replace paths in 'Routes.js' with their convenient constants

--- src/Routes.js
+++ src/Routes.js
@@ -1,45 +1,46 @@
 import { Route, Switch } from "react-router";
 import { Main as MainLayout } from "./layout";
+import * as route from "./constants/routes";
 
 export function Routes(props) {
   return <Switch>
-    <Route path="/" exact>
+    <Route path={ route.MAIN_PATH } exact>
       <MainLayout page="Main" />
     </Route>
 
-    <Route path="/about">
+    <Route path={ route.ABOUT_PATH }>
       <MainLayout page="About" />
     </Route>
 
-    <Route path="/services">
+    <Route path={ route.SERVICES_PATH }>
       <MainLayout page="Services" />
     </Route>
 
-    <Route path="/staff">
+    <Route path={ route.STAFF_PATH }>
       <MainLayout page="Staff" />
     </Route>
 
-    <Route path="/login">
+    <Route path={ route.LOGIN_PATH }>
       <MainLayout page="Login" />
     </Route>
 
-    <Route path="/signup">
+    <Route path={ route.SIGNUP_PATH }>
       <MainLayout page="Signup" />
     </Route>
 
-    <Route path="/check-email">
+    <Route path={ route.CHECK_EMAIL_PATH }>
       <MainLayout page="CheckEmail" />
     </Route>
 
-    <Route path="/new-password">
+    <Route path={ route.NEW_PASSWORD_PATH }>
       <MainLayout page="NewPassword" />
     </Route>
 
-    <Route path="/counters">
+    <Route path={ route.COUNTERS_PATH }>
       <MainLayout page="Counters" />
     </Route>
 
-    <Route path="/users">
+    <Route path={ route.USERS_PATH }>
       <MainLayout page="Users" />
     </Route>
   </Switch>;
