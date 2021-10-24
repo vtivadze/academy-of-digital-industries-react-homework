886ccdd7fa98388895579bc76dfcea4f63f3e19c
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-24 20:29:28 +0400

    Check for existens user authentication status in local storage

--- src/atoms/SecureComponent/SecureComponent.js
+++ src/atoms/SecureComponent/SecureComponent.js
@@ -1,8 +1,10 @@
 import { Redirect } from "react-router";
 import { ACCESS_IS_DENIED_PATH } from "../../constants/routes";
+import { AUTH_TOKEN } from "../../constants/constants";
+import { itemExists } from "../../helpers/localStorage";
 
 export const SecureComponent = ({ Page, ...props }) => {
-  const loggedIn = true;
+  const loggedIn = itemExists(AUTH_TOKEN);
 
   return loggedIn ? <Page {...props} /> : <Redirect to={ ACCESS_IS_DENIED_PATH } />;
 };
