374a5d8e8e3830ae9918f6a864e27cf0b1f0ff83
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-24 13:48:55 +0400

    Homework-5: Realize redirect protected 'Profile' page to 'AccessIsDenied' page

--- src/atoms/SecureComponent/SecureComponent.js
+++ src/atoms/SecureComponent/SecureComponent.js
@@ -1,5 +1,8 @@
+import { Redirect } from "react-router";
+import { ACCESS_IS_DENIED_PATH } from "../../constants/routes";
+
 export const SecureComponent = ({ children }) => {
   const loggedIn = false;
 
-  return loggedIn ? children : null;
+  return loggedIn ? children : <Redirect to={ ACCESS_IS_DENIED_PATH } />;
 };
