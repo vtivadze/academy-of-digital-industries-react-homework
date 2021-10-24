b3dd1d6327696a5cc016f4d3409b2e8cee75dc2f
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-24 04:39:49 +0400

    Create rout for main page and add 'exact' attribute to it's 'Route' component

--- src/Routes.js
+++ src/Routes.js
@@ -1,3 +1,13 @@
+import { Route, Switch } from "react-router";
+import { Main } from "./layout";
+
 export function Routes(props) {
-  return null;
+  return <Switch>
+    <Route path="/" exact>
+      <Main
+        content={props.content}
+        customButtonClickHandler={props.customButtonClickHandler}
+      />
+    </Route>
+  </Switch>;
 };
