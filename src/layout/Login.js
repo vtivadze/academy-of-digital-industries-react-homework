43b11135f25795ff800c406983d52d8538e3d07c
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-24 20:40:57 +0400

    Make displaying 'Logout' button depending user authorization status

--- src/layout/Login.js
+++ src/layout/Login.js
@@ -1,23 +1,39 @@
-import { NavLink } from "react-router-dom";
 import { LOGIN_PATH, REGISTER_PATH } from "../constants/routes";
-export const Login = (props) => {
+export const Login = () => {
+  const loggedIn = false;
+
+  const renderGuestLinks = () => {
+    return (
+      <>
+        <button
+          className="button is-info mr-0 mb-0"
+          to={ REGISTER_PATH }
+          activeClassName="has-background-dark"
+        >
+          <strong>Register</strong>
+        </button>
+        <button
+          className="button is-info is-align-self-stretch mr-0"
+          to={ LOGIN_PATH }
+          activeClassName="has-background-dark"
+        >
+          <strong>Log in</strong>
+        </button>
+      </>
+    );
+  };
+
+  const renderUserLinks = () => {
+    return (
+      <button className="button is-info mr-0 mb-0">
+        <strong>Log Out</strong>
+      </button>
+    );
+  };
 
   return (
     <div className="buttons is-flex-direction-column-reverse mb-0">
-      <NavLink
-        className="button is-info mr-0 mb-0"
-        to={ REGISTER_PATH }
-        activeClassName="has-background-dark"
-      >
-        <strong>Register</strong>
-      </NavLink>
-      <NavLink
-        className="button is-info is-align-self-stretch mr-0"
-        to={ LOGIN_PATH }
-        activeClassName="has-background-dark"
-      >
-        <strong>Log in</strong>
-      </NavLink>
+      { loggedIn ? renderUserLinks() : renderGuestLinks() }
     </div>
   );
 };
