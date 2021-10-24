66055f7eae9956ec735c5ff26aece54b27a8171b
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-24 11:51:55 +0400

    Homework-5: Change paths in 'mainMenuItems.js' with their convenient constants

--- src/data/mainMenuItems.js
+++ src/data/mainMenuItems.js
@@ -1,6 +1,8 @@
+import * as route from "../constants/routes";
+
 export const mainMenuItems = [
-  ["Main", "/",true],
-  ["About", "/about",false],
-  ["Services", "/services",false],
-  ["Staff", "/staff",false],
+  ["Main", route.MAIN_PATH, true],
+  ["About", route.ABOUT_PATH, false],
+  ["Services", route.SERVICES_PATH, false],
+  ["Staff", route.STAFF_PATH, false],
 ];
