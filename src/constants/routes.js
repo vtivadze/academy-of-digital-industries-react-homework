ec50a1580e5ad68a99d71d3f2424d6c1f06455dc
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-24 11:41:48 +0400

    Homework-5: Create constants for routes

---
+++ src/constants/routes.js
@@ -0,0 +1,10 @@
+export const MAIN_PATH = "/";
+export const ABOUT_PATH = "/about";
+export const SERVICES_PATH = "/services";
+export const STAFF_PATH = "/staff";
+export const LOGIN_PATH = "/login";
+export const SIGNUP_PATH = "/signup";
+export const CHECK_EMAIL_PATH = "/check-email";
+export const NEW_PASSWORD_PATH = "/new-password";
+export const COUNTERS_PATH = "/counters";
+export const USERS_PATH = "/users";
