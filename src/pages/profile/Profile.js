0d80e3f5cceb104ee5d2a25e47f869f5cc416726
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-25 01:52:18 +0400

    Example of using 'cleanup functions' in 'useEffect' hook's

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+
 export const Profile = (props) => {
+  useEffect(() => {
+    document.body.style.background = "yellow";
+    return () => {
+      // cleanup function
+      document.body.style.background = "initial";
+    };
+  },[]);
+
   return (
     <>
       <h1 className="title">Profile Page</h1>
