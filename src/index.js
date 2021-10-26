c05c791152e875a514e83e1e4fd2d9c3c576fa8d
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-26 05:52:06 +0400

    Add default URL for 'axios' in 'src/index.js'

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 // import React from "react";
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { axios } from 'axios';
 import { AuthProvider } from './providers/AuthProvider';
 import './index.css';
 import { App } from './App';
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 ReactDOM.render(
   <BrowserRouter>
