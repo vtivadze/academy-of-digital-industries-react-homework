fd99f7897364f54b736a166f54728cd269ff5a46
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-28 13:35:43 +0400

    Create hook 'useFetch'

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,3 +1,37 @@
-export const useFetch = () => {
+import { useState, useEffect } from "react";
 
+export const useFetch = (initialUrl, initialOptions) => {
+  const [url, setUrl] = useState(initialUrl);
+  const [options, setOptions] = useState(initialUrl);
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(undefined);
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        setData(result);
+      }
+      catch(error) {
+        setError(error);
+      }
+      finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+
+    return () => {
+      console.log("CLEAN USE FETCH");
+    }
+
+  }, [url, options]);
+
+  return {data, error, loading, setUrl, setOptions};
 };
