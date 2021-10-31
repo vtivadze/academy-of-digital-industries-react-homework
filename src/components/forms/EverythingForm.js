81595680bbde2fa72986de131bec1e7674d3ed1b
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-31 09:07:42 +0400

    Add inputs for 'EverythingForm'

--- src/components/forms/EverythingForm.js
+++ src/components/forms/EverythingForm.js
@@ -1,23 +1,36 @@
-import { DateInput } from "../../atoms/DateInput/DateInput";
+import { DateInput } from "../../atoms";
+import { SelectInput } from "../../atoms/SelectInput/SelectInput";
 
 export const EverythingForm = () => {
   const onSubmit = event => event.preventDefault();
 
   return (
-    <form
-      className="box column is-half is-offset-one-quarter mt-6"
-      onSubmit={onSubmit}>
+    <div className="box column is-10 is-offset-1 mt-6">
       <h1 className="title has-text-centered">Everything</h1>
+      <form onSubmit={onSubmit}>
+        <div className="columns">
+          <div className=" column field is-horizontal">
+            <label class="label mr-3">From</label>
+            <DateInput inputName="from" />
+          </div>
 
-      <div className="field">
-        <DateInput inputName="from" />
-      </div>
+          <div className="column field is-horizontal">
+            <label class="label mr-3">To</label>
+            <DateInput inputName="to" />
+          </div>
 
-      <div className="field tile is-justify-content-space-between">
-        <p className="control">
-          <button className="button is-success">Get News</button>
-        </p>
-      </div>
-    </form>
+          <div className="column field is-horizontal">
+            <label class="label mr-3">Sort By</label>
+            <SelectInput options={["", "relevancy", "popularity", "publishedAt"]} />
+          </div>
+        </div>
+
+        <div className="field tile is-justify-content-space-between">
+          <p className="control">
+            <button className="button is-success">Get News</button>
+          </p>
+        </div>
+      </form>
+    </div>
   );
 };
