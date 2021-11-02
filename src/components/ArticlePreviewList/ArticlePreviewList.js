78dd81d00ae52ac7696c7905981fd59807ed21b9
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-11-02 18:21:44 +0400

    Add link and little improve design for the 'News' page searching result

--- src/components/ArticlePreviewList/ArticlePreviewList.js
+++ src/components/ArticlePreviewList/ArticlePreviewList.js
@@ -1,6 +1,12 @@
 export const ArticlePreviewList = ({articles}) => {
 
   return articles.map((article, index) => {
-      return <h2 className="title is-size-5" key={index}>{article.title}</h2>
-    });
+    return (
+      <div className="notification">
+        <a href={article.url} target="_blank" rel='noreferrer'>
+          <h3 className="title is-size-5" key={index}>{article.title}</h3>
+        </a>
+      </div>
+    );
+  });
 };
