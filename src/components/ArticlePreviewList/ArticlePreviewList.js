9efb785bdc9c10601e2048b12174c8cb44e30c50
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-11-04 14:28:30 +0400

    Move 'key' attribute from 'h3' to upper parent 'div' element in 'ArticlePreviewList.js'

--- src/components/ArticlePreviewList/ArticlePreviewList.js
+++ src/components/ArticlePreviewList/ArticlePreviewList.js
@@ -1,9 +1,9 @@
 export const ArticlePreviewList = ({ articles }) => {
   return articles.map((article, index) => {
     return (
-      <div className="notification">
+      <div className="notification"  key={index}>
         <a href={article.url} target="_blank" rel="noreferrer">
-          <h3 className="title is-size-5" key={index}>
+          <h3 className="title is-size-5">
             {article.title}
           </h3>
         </a>
